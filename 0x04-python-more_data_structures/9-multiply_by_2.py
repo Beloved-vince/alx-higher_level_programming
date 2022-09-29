509ca4a538201d7b4bf3cb7e159ca7c1a7b40f9e
Beloved-vince <vinceoludare@gmail.com>
2022-09-29 09:23:19 +0100

    getting multiples of value

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dict = sorted(a_dictionary)
-    for i in new_dict:
-        if new_dict[i] is not None
-            print("{}: {}".format(i, new_dict[i] * 2))
+    for i in a_dictionary:
+        new_dict[i] = a_dictionary[i] * 2
+    return new_dict
