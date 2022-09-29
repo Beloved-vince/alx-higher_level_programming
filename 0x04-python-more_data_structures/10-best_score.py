3666d9b059c8c819d89557232bebb1fed72216c8
Beloved-vince <vinceoludare@gmail.com>
2022-09-29 09:32:46 +0100

    key with highest

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not in a_dictionary:
+    if not a_dictionary:
         return None
     else:
         return max(a_dictionary, key=lambda x: a_dictionary[x])
