8b820d1da54ec5f83113ed01b30542e877392e70
Beloved-vince <vinceoludare@gmail.com>
2022-10-10 13:51:33 +0100

    only integer in list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             ret += 1
-        except (IndexError, ValueError, TypeError):
+        except (ValueError, TypeError):
             pass
     print("", end="\n")
     return ret
