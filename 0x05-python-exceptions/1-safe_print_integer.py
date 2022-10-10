f5a4c92fd0bc54b8552c8a5e380226808a730830
Beloved-vince <vinceoludare@gmail.com>
2022-10-10 13:27:47 +0100

    integer only

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        pass
-    except ValueError:
-        pass
+        return True
+    except (ValueError, TypeError):
+        return False
