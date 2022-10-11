1f550ab10970f6d2f2f409e06da2ae144ef05f8d
Beloved-vince <vinceoludare@gmail.com>
2022-10-11 10:00:06 +0100

    executing program by ommiting error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except Exception as error_msg: 
+    except Exception as error_msg:
         sys.stderr.write("Exception: " + str(error_msg) + "\n")
         return None
