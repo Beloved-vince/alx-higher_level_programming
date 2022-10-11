5c1624d2089820c021d63c0e96d77595a70b6dcd
Beloved-vince <vinceoludare@gmail.com>
2022-10-12 00:19:58 +0100

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,18 +10,18 @@ class Square:
     pass
 
     def __init__(self, size=0):
-    """
-    init method
-    :param size: size of the Square
-    Args:
-    size: size of the Square
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
-                                             
+        """
+        init method
+        :param size: size of the Square
+        Args:
+        size: size of the Square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
     def area(self):
         """returns the size square"""
         return self.__size ** 2
