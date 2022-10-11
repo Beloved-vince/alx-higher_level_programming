b955ab0e4015b7c75c237d084ef107a98f2136df
Beloved-vince <vinceoludare@gmail.com>
2022-10-11 23:01:30 +0100

    private instance attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+"""To define the private instance of the class"""
+
+
+class Square:
+    """square class"""
+    pass
+
+    def __init__(self, size):
+        """
+        init method
+        :param size: size of the square
+
+        Args:
+            size: size of the Square
+        """
+        self.__size = size
