b25f15d2814c7284c0507f8a645dbd92b5346e46
Beloved-vince <vinceoludare@gmail.com>
2022-10-25 21:04:49 +0100

    json decode type

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,19 +1,17 @@
-#!/usr/bin//python3
+#!/usr/bin/python3
 """
-    Module return object represented by JSON strin
-
-    return type and format
+Module 6-from_json_string
+Contains function that returns python data structure from JSON string
 """
-
-
-import json
 
 
 def from_json_string(my_str):
-    """ Modules get the decoded file
-
-        Return: json format
-        
-        Args: my_str data object
+    """Returns python data structure from JSON string
+    Args:
+        my_str: json string representation
+    Return:
+        python object
     """
+    import json
+
     return json.loads(my_str)
