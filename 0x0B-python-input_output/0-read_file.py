b444ee7b95a55cc5391e042fa2fd7a1d96c12561
Beloved-vince <vinceoludare@gmail.com>
2022-10-25 19:58:12 +0100

    read file in utf-8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
     Module: fucnction to read a text file
+
+    Contains function that reads and prints contents from file
 """
 
 
 def read_file(filename=''):
+    """ Reading file in utf-8 version """
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read())
