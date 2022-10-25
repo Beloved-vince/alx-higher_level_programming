76d9e99d3a90f9ccb43e44d2cdd71ab2b93b6697
Beloved-vince <vinceoludare@gmail.com>
2022-10-25 20:59:52 +0100

    json string representation format

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""
+    Module: JSON representation of an object
+
+    return json representation of string
+"""
+
+
+def to_json_string(obj):
+    """Json format
+        Args: json return json string format
+    """
+    return json.dumps(obj)
