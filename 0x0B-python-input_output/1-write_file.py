1d318b7fc83349fc3902338453d6d6f21f0e2102
Beloved-vince <vinceoludare@gmail.com>
2022-10-25 21:36:49 +0100

    append

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,10 @@
 
 
 def write_file(filename="", text=""):
+    """Return filename by appending text to the end of filename
+        Args:
+            filename: file
+            text: text to be appended
+    """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
