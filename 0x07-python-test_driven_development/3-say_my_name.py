6ac9a38c98de34674c9292ca8fd41886fd1be3d3
Beloved-vince <vinceoludare@gmail.com>
2022-11-02 12:10:16 +0100

    string only

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-
+"""
+This function get name and last name in string format only
+first_name in string litera
+Also last_name in string literal
+"""
 def say_my_name(first_name, last_name=""):
+    """
+    return func in string literal
+    """
     if type(first_name) is not str:
         raise TypeError ("first_name must be a string")
     if  type(last_name) is not str:
