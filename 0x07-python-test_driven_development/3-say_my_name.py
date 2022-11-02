78c1d919cf03301329c1efbf02c25eb218b1588b
Beloved-vince <vinceoludare@gmail.com>
2022-11-02 12:13:33 +0100

    string only

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,13 +4,15 @@ This function get name and last name in string format only
 first_name in string litera
 Also last_name in string literal
 """
+
+
 def say_my_name(first_name, last_name=""):
     """
     return func in string literal
     """
     if type(first_name) is not str:
-        raise TypeError ("first_name must be a string")
-    if  type(last_name) is not str:
-        raise TypeError ("last_name must be a string")
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
     else:
         print(f"My name is {first_name} {last_name}")
