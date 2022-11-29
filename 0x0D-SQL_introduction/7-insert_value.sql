d303d34d22a1bf4b22d6a120d050d12f2a1ff4dd
Beloved-vince <vinceoludare@gmail.com>
2022-11-29 19:16:54 +0100

    list record

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- This script insert a new row in specified table
 -- +present in MySQL server
 
-INSERT INTO first_table(id, name) VALUES(89, "Best SCHOOL)";
+INSERT INTO first_table(id, name) VALUES(89, "Best SCHOOL");
