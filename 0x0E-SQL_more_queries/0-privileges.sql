3633c5701f8e6a5fc4227ce2129ab98475707d64
Beloved-vince <vinceoludare@gmail.com>
2022-11-30 17:38:10 +0100

    users with privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- List all users granted privileges to a database
+-- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
 
-SHOW GRANTS FOR 'user_0d_1','user_0d_2'@localhost;
-
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
