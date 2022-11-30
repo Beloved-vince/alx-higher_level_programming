2ac8aa9755783257bcb4095ec751626de85291f4
Beloved-vince <vinceoludare@gmail.com>
2022-11-30 21:01:44 +0100

    nested users/database

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+-- run this first to import a SQL dump -->
+   SELECT tv_shows.title, tv_show_genres.genre_id
+     FROM tv_shows
+LEFT JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+ ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,15 @@
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows
+-- tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- each record should display: tv_shows.title
+-- results must be sorted in ascending order by the show title
+-- can use only one SELECT statement
+-- the database name will be passed as an argument of the mysql command
+
+    SELECT tv_shows.title
+      FROM tv_shows
+INNER JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+     WHERE tv_genres.name = "Comedy"
+  ORDER BY tv_shows.title ASC;
