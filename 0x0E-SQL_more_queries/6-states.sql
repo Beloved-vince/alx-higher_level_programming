ef8b04cc2de1f41f1f011c003b2d0866eb5f9d2b
Beloved-vince <vinceoludare@gmail.com>
2022-11-30 20:44:17 +0100

    nested users/database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,4 +3,5 @@
 -- (in the database hbtn_0d_usa) on your MySQL server.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(id INT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
