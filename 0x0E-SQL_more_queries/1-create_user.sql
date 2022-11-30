4c5844be4939c60bffcbc3e689183b706be44011
Beloved-vince <vinceoludare@gmail.com>
2022-11-30 19:18:25 +0100

    create users

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- A  script that creates the MySQL server user user_0d_1.
+-- user_0d_1 should have all privileges on your MySQL server
+-- The user_0d_1 password should be set to user_0d_1_pwd
+-- If the user user_0d_1 already exists, your script should not fail
+
+IF EXISTS CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_od_1_pwd';
