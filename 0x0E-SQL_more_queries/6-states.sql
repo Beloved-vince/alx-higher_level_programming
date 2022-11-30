cf53b963608075fa47fd06a7c77ba53717e4e312
Beloved-vince <vinceoludare@gmail.com>
2022-11-30 20:43:11 +0100

    nested users/database

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Nested Database
+-- A script that creates the database hbtn_0d_usa and the table states
+-- (in the database hbtn_0d_usa) on your MySQL server.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
