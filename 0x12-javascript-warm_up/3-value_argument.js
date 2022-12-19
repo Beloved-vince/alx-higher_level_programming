2626be99f6684e470b9a402909f81a8476e2157c
Beloved-vince <vinceoludare@gmail.com>
2022-12-19 23:20:16 +0100

    javascript

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 2) {
-    console.log("No argument");
-}else if (process.argv.length === 3){
-    console.log("Argument found");
-}else {
-    console.log("Argument found");
-}
+if (process.argv[2] === undefined) {
+    console.log('No argument');
+  } else {
+    console.log(process.argv[2]);
+  }
