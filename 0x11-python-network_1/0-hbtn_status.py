76a95b181456511f69359cd6cec8391dc954e14d
Beloved-vince <vinceoludare@gmail.com>
2023-01-17 18:47:51 +0100

    commit:code dump

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,12 @@
 This  Python script that fetches https://alx-intranet.hbtn.io/status
 Using  the package urllib
 """
+
 import urllib.request
 
 if __name__ == '__main__':
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        result = response.read()
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as html:
+        result = html.read()
         print("Body response:")
         print("\t - type: {}".format(type(result)))
         print('\t - content: {}'.format(result))
