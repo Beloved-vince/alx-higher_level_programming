9af54083706231d226ec9ae8d1354b4880f3601c
Beloved-vince <vinceoludare@gmail.com>
2023-01-17 20:38:07 +0100

    commit stash

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """
-given URL & email as params, send POST req to URL, display response body utf-8
-usage: ./2-post_email.py http://0.0.0.0:5000/post_email hr@holbertonschool.com
+Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
 """
 from sys import argv
-import urllib.parse
 import urllib.request
 
 
-if __name__ == '__main__':
-    url = argv[1]
-    request = urllib.request.Request(url)
+if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(request) as resp:
-            print(resp.read().decode('utf-8'))
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
