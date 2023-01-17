44497a71aea080f6bd802c3281d4e9a84d9dbaa5
Beloved-vince <vinceoludare@gmail.com>
2023-01-17 20:09:39 +0100

    POST request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
 Python script that takes in a URL and an email, sends a POST
-request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8)
 """
 
 from sys import argv
