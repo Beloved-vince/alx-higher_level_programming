db9f8a58016fe5f75aed18498d4eb2c450d7f45d
Beloved-vince <vinceoludare@gmail.com>
2023-01-17 21:53:49 +0100

    commit stash

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the value of the
+variable Email in the response header
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.post(argv[1], data={"key":argv[2]})
+    print("Your email is".format(r.headers.get(argv[2])))
