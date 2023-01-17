9e717b199b41e1bdd933477418fc94612ac9f6a9
Beloved-vince <vinceoludare@gmail.com>
2023-01-17 21:00:35 +0100

    commit stash

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,10 @@
 """
 Fetches https://intranet.hbtn.io/status
 use the package urllib
- body of the response must be displayed in tabulation before -
+body of the response must be displayed in tabulation before -
 """
 import requests
+
 r = requests.get('https://alx-intranet.hbtn.io/status')
 resp = r.text
 print('Body response:\n\t- type: {}'.format(type(resp)))
