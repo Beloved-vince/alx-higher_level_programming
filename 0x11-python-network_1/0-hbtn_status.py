7f591df8342b3b4046face6b63c893d4426ac476
Beloved-vince <vinceoludare@gmail.com>
2023-01-17 19:02:44 +0100

    commit:code dump

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
         content = resp.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("    - type: {}".format(type(content)))
+        print("    - content: {}".format(content))
+        print("    - utf8 content: {}".format(content.decode('utf-8')))
