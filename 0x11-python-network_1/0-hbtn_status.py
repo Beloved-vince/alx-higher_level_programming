b62b3c0494cc1c94c1ad4cd8e6d1226137c4fac0
Beloved-vince <vinceoludare@gmail.com>
2023-01-17 18:50:24 +0100

    commit:code dump

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-This  Python script that fetches https://alx-intranet.hbtn.io/status
-Using  the package urllib
+Python script fetch https://intranet.hbtn.io/status; display response
 """
 
 import urllib.request
 
-if __name__ == '__main__':
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as html:
-        result = html.read()
-        print("Body response:")
-        print("\t - type: {}".format(type(result)))
-        print('\t - content: {}'.format(result))
-        print("\t - utf8 content: {}".format(result.decode("utf8")))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
