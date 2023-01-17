e67f65ae27f2c64fd95c68e10e161b666505ffd4
Beloved-vince <vinceoludare@gmail.com>
2023-01-17 18:45:38 +0100

    commit:code dump

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,10 @@ Using  the package urllib
 """
 import urllib.request
 
-
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    result = response.read()
-    print("Body response:")
-    print("\t - type: {}".format(type(result)))
-    print('\t - content: {}'.format(result))
-    print("\t - utf8 content: {}".format(result.decode("utf8")))
+if __name__ == '__main__':
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        result = response.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(result)))
+        print('\t - content: {}'.format(result))
+        print("\t - utf8 content: {}".format(result.decode("utf8")))
