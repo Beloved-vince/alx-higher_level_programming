1ea2ea224a316497b675271782233fe37312ff8b
Beloved-vince <vinceoludare@gmail.com>
2023-01-17 21:35:24 +0100

    commit stash

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     from sys import argv
     url = argv[1]
     req = requests.get(url)
-    print(dict(req.headers).get('X-Request-Id'))
+    print(req.headers.get('X-Request-Id'))
