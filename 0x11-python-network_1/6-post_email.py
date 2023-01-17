20f4ff8443679449a08a514f54513c92eb57f6da
Beloved-vince <vinceoludare@gmail.com>
2023-01-17 22:00:05 +0100

    commit stash

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays the value of the
-variable Email in the response header
+takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
 """
 if __name__ == '__main__':
     import requests
     from sys import argv
-    r = requests.post(argv[1], data={"key": argv[2]})
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
     print(r.text)
