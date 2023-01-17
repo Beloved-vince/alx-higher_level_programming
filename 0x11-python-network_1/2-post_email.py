5f7300183a9a43b47fc9d133dd69ba3022c93678
Beloved-vince <vinceoludare@gmail.com>
2023-01-17 20:01:27 +0100

    POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+from sys import argv
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email': argv[2]}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
